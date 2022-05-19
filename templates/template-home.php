6dbb90eb32dfdd1dbf03c190493b180328db7779
MarceloATabone <marceloatabone@hotmail.com>
2022-05-18 21:21:29 -0300

    change (get_header and get_footer for template)

--- templates/template-home.php
+++ templates/template-home.php
@@ -1,5 +1,12 @@
 <?php
-/**
- * Template Name: Home LandPage
- * Template Post Type: page
- */
+	/**
+	 * Template Name: Home LandPage
+	 * Template Post Type: page
+	 */
+	 get_header(); 
+ ?>
+
+
+
+
+<?php get_footer(); ?>
